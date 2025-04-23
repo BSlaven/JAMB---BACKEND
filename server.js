d0437e6130f5b2eb8c62c912f644e7e81b0146a0
BSlaven <slaven.bunijevac@gmail.com>
2025-04-23 23:30:36 +0200

    add express library

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+io.on('connection', (socket) => {
+  console.log('a user connected:', socket.id);
+
+  // Listen for messages from client
+  socket.on('chat message', (msg) => {
+    console.log('message:', msg);
+    io.emit('chat message', msg); // Broadcast to all clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Majstore, taj server ti je pokrenut!', socket.id);
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
