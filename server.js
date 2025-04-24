7f3e67700aaa4d3189f70b91aef3cbf574498654
BSlaven <slaven.bunijevac@gmail.com>
2025-04-24 15:52:55 +0200

    add real localhost url for testing

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  }
+});
 
 io.on('connection', (socket) => {
   console.log('a user connected:', socket.id);
 
-  socket.on('chat message', (msg) => {
-    console.log('message:', msg);
-    io.emit('chat message', msg);
+  socket.on('opponentData', (data) => {
+    console.log('Opponent Data:', data);
+    io.emit('Opponent Data', data);
   });
 
   socket.on('disconnect', () => {
-    console.log('Majstore, taj server ti je pokrenut!', socket.id);
+    console.log('Majstore, taj server ti je prekinut!', socket.id);
   });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Majstore ovaj server ti je na http://localhost:${PORT}`);
 });
